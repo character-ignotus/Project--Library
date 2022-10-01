7866231d864c72c468462c7b443313e581dd6237
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-10-02 01:00:10 +0300

    Add a basic function that takes user inputs

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function addBookToLibrary() {
+    const title = prompt('Title');
+    const author = prompt('Author');
+    const pages = +prompt('Pages');
+    const read = Boolean(prompt('Read'));
+    myLibrary.push(new Book(title, author, pages, read));
+}
