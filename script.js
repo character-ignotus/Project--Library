e655bfdd97ff4ef1980aa3881ad51fcd6c25edc3
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-10-03 00:33:29 +0300

    Add a remove button on each book's card

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function addBookToLibrary() {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = +document.querySelector('#pages').value;
-    const read = Boolean(document.querySelector('#read').value);
+    const read = document.querySelector('#read').value;
     myLibrary.push(new Book(title, author, pages, read));
 }
 
@@ -42,9 +42,30 @@ submitBook.addEventListener('click', () => {
         let pages = document.createElement('p');
         pages.textContent = myLibrary[myLibrary.length - 1].pages;
 
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+
+    removeBtn.addEventListener('click', (e) => {
+        console.log(e.target.parentElement);
+
+        let length = myLibrary.length;
+        let currentCard = e.target.parentElement;
+        let currentCardNum = e.target.parentElement.getAttribute('data');
+        myLibrary.splice(currentCardNum, 1);
+
+        for(let i = currentCardNum; i < length; i++) {
+            let div = document.querySelector(`[data="${i}"]`);
+            div.setAttribute('data', `${i - 1}`);
+            console.log(div.getAttribute);
+        }
+
+        bookCardsSection.removeChild(currentCard);
+    });
+
     cardDiv.appendChild(para);
     cardDiv.appendChild(author);
     cardDiv.appendChild(pages);
+    cardDiv.appendChild(removeBtn);
     bookCardsSection.appendChild(cardDiv);
 
     inputs.forEach(input => {
@@ -60,3 +81,4 @@ openModal.addEventListener('click', () => {
 closeModal.addEventListener('click', () => {
     modal.close();
 });
+
